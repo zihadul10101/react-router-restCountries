16bbbd6ab73d554c85a9fbcf41380e263da8099d
unknown <you@example.com>
2021-03-08 17:30:31 +0600

    style not ok

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
 
 const Country = (props) => {
     //console.log(props.country);
     const {name,flag} =props.country;
     const flagStyle ={height:'50px'}
     const friendStyle = {
-        border: '1px solid purple',
-        margin: '20px',
-        padding: '20px',
-        borderRadius: '20px'
+        // border: '1px solid purple',
+        // margin: '20px',
+        // padding: '20px',
+        // borderRadius: '20px'
+        // boxShadow:' 5px 5px blue',
+        border: '3px solid purple',
+        width:'20%',
+        display:'grid'
       }
    
     return (
         <div style={friendStyle} >
-           <img style={flagStyle} src={flag} alt=""/>
-            <h2>name: {name}</h2> 
-          <button > <Link to={`/${name}`}>show detail </Link></button>
+          <Card>
+    <Card.Img variant="top" style={flagStyle} src={flag}/>
+    <Card.Body>
+      <Card.Title><h2>name: {name}</h2></Card.Title>
+      <Card.Text>
+      <button > <Link to={`/${name}`}>show detail </Link></button>
+      </Card.Text>
+    </Card.Body>
+    
+  </Card>
             
         </div>
     );
--- src/components/CountryDetiles/CountryDetiles.js
+++ src/components/CountryDetiles/CountryDetiles.js
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Card } from 'react-bootstrap';
 
 const CountryDetiles = () => {
-    const {name} =useParams();
-    const [country, setCountry] =useState([{}]);
-    
-   console.log(country);
-    
-    useEffect(()=>{
-        const url=`https://restcountries.eu/rest/v2/name/${name}`;
+    const { name } = useParams();
+    const [country, setCountry] = useState([{}]);
+
+    //  console.log(country);
+
+    useEffect(() => {
+        const url = `https://restcountries.eu/rest/v2/name/${name}`;
         fetch(url)
-        .then(res => res.json())
-        .then(data=>setCountry(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setCountry(data));
+    }, [])
+
+   const CountryDetilesStyle = { display: 'flex' };
     return (
-        <div>
-            <h1>country name:{name}</h1>
-           <h2>capital: {country[0].capital}</h2>
-           <h2>nativeName: {country[0].nativeName}</h2>
-           <h2>population: {country[0].population}</h2>
-           <h2>area: {country[0].area}</h2>
-        
+        <div style={CountryDetilesStyle}>
+            {/* <h1>country name:{name}</h1>
+            <h2>capital: {country[0].capital}</h2>
+            <h2>nativeName: {country[0].nativeName}</h2>
+            <h2>population: {country[0].population}</h2>
+            <h2>area: {country[0].area}</h2> */}
+
+            <Card.Body>
+
+                <Card.Title><h1>country name: {name}</h1></Card.Title>
+                <Card.Text>
+                    <h2>capital: {country[0].capital}</h2>
+                    <h2>nativeName: {country[0].nativeName}</h2>
+                    <h2>population: {country[0].population}</h2>
+                    <h2>area: {country[0].area}</h2>
+                </Card.Text>
+
+            </Card.Body>
+
+
 
         </div>
     );
